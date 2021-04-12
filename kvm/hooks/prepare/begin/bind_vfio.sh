cd5473740824969bba7074d38b83b771b0bfcbd3
Felipe Cavalcanti <fjfcavalcanti@gmail.com>
2021-04-12 14:40:35 -0300

    Added my win10 and macOS passthrough xmls and fixes
    Added my optimized files for b450 and RX5700 GPU, include a fix for the
    reset bug and resume bug

--- kvm/hooks/prepare/begin/bind_vfio.sh
+++ kvm/hooks/prepare/begin/bind_vfio.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
-## Load the config file
 source "/etc/libvirt/hooks/kvm.conf"
 
-## Unload nvidia
-modprobe -r nvidia_drm
-modprobe -r nvidia_uvm
-modprobe -r nvidia_modeset
+## Kill the display manager
+echo "Killing display manager"
+systemctl stop display-manager.service
+
+sleep 3
+
+## Kill the console
+echo 0 > /sys/class/vtconsole/vtcon0/bind
+echo 0 > /sys/class/vtconsole/vtcon1/bind
+
+sleep 2
+
+echo "Unloading AMD driver and binding devices to VFIO"
+## Unload amd
+modprobe -r amdgpu
 
 ## Load vfio
 modprobe vfio
@@ -14,7 +24,20 @@ modprobe vfio_iommu_type1
 modprobe vfio_pci
 
 ## Unbind gpu from nvidia and bind to vfio
-virsh nodedev-detach $VIRSH_GPU_VIDEO
-virsh nodedev-detach $VIRSH_GPU_AUDIO
-## Unbind ssd from nvme and bind to vfio
-virsh nodedev-detach $VIRSH_NVME_SSD
+virsh nodedev-detach pci_0000_${VIRSH_GPU_VIDEO//[:.]/_}
+virsh nodedev-detach pci_0000_${VIRSH_GPU_AUDIO//[:.]/_}
+virsh nodedev-detach pci_0000_${VIRSH_GPU_USB//[:.]/_}
+virsh nodedev-detach pci_0000_${VIRSH_HDA//[:.]/_}
+
+# This step is very important, not only it prevends AMD reset bug and code 49 bug, it also makes it possible for the host to rebind successfully to the GPU
+# Uncomment if needed
+#echo "Reseting AMD GPU so that we prevent a log of bugs in both host and vm"
+#echo "disconnecting amd graphics"
+#echo "1" | tee -a /sys/bus/pci/devices/0000\:${VIRSH_GPU_VIDEO}/remove
+#echo "disconnecting amd sound counterpart"
+#echo "1" | tee -a /sys/bus/pci/devices/0000\:${VIRSH_GPU_AUDIO}/remove
+#echo "will go to sleep now for 5 seconds"
+#rtcwake -m mem -s 5
+#echo "reconnecting amd gpu and sound counterpart"
+#echo "1" | tee -a /sys/bus/pci/rescan
+#echo "AMD graphics card sucessfully reset"
--- kvm/hooks/release/end/unbind_vfio.sh
+++ kvm/hooks/release/end/unbind_vfio.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
 
-## Load the config file
 source "/etc/libvirt/hooks/kvm.conf"
 
+echo "Unloading VFIO so that amdgpu can bind into the GPU again"
 ## Unbind gpu from vfio and bind to nvidia
-virsh nodedev-reattach $VIRSH_GPU_VIDEO
-virsh nodedev-reattach $VIRSH_GPU_AUDIO
-## Unbind ssd from vfio and bind to nvme
-virsh nodedev-reattach $VIRSH_NVME_SSD
+virsh nodedev-reattach pci_0000_${VIRSH_GPU_VIDEO//[:.]/_}
+virsh nodedev-reattach pci_0000_${VIRSH_GPU_AUDIO//[:.]/_}
+virsh nodedev-reattach pci_0000_${VIRSH_GPU_USB//[:.]/_}
+virsh nodedev-reattach pci_0000_${VIRSH_HDA//[:.]/_}
 
 ## Unload vfio
 modprobe -r vfio_pci
 modprobe -r vfio_iommu_type1
 modprobe -r vfio
 
-## Load nvidia
-modprobe nvidia_modeset
-modprobe nvidia_uvm
-modprobe nvidia_drm
+# This step is very important, not only it prevends AMD reset bug and code 49 bug, it also makes it possible for the host to rebind successfully to the GPU
+# Uncomment if needed
+#echo "Reseting AMD GPU so that we prevent a log of bugs in both host and vm"
+#echo "disconnecting amd graphics"
+#echo "1" | tee -a /sys/bus/pci/devices/0000\:${VIRSH_GPU_VIDEO}/remove
+#echo "disconnecting amd sound counterpart"
+#echo "1" | tee -a /sys/bus/pci/devices/0000\:${VIRSH_GPU_AUDIO}/remove
+#echo "will go to sleep now for 5 seconds"
+#rtcwake -m mem -s 5
+#echo "reconnecting amd gpu and sound counterpart"
+#echo "1" | tee -a /sys/bus/pci/rescan
+#echo "AMD graphics card sucessfully reset"
+#
+#sleep 5
+
+systemctl start display-manager.service
